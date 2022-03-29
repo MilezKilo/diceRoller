11470793f4f43a8c8d68078c6b9ab83e4a2d627e
MaksPonizov <76489164+MilezKilo@users.noreply.github.com>
2022-03-29 20:25:11 +0300

    Code Refactoring
    Reducing the amount of code by half, more readable code.
    Add simple input protection.

--- diceRoller.py
+++ diceRoller.py
@@ -1,99 +1,56 @@
 from random import randrange
-
-# 6 - Выбрасыватель игральных костей
-# Dungeons & Dragons и других настольных ролевых играх используются специальные игральные кости с 4, 8, 10, 12 или даже 20 гранями.
-# В этих играх есть также специальные обозначения для бросков различных костей.
-# Например, 3d6 означает выбрасывание трех шестигранных костей, а 1d10+2 — выбрасывание одной десятигранной кости с добавлением к броску бонуса в два очка.
-# Представленная ниже программа моделирует подобные броски костей на случай, если вы забыли захватить с собой свои.
-# Она также моделирует выбрасывание не существующих физически костей, например 38-гранных.
-# Программа в действии.
-# Результат выполнения diceroller.py выглядит следующим образом:
-# --сокращено--
-# >	3d6
-# 7 (3, 2, 2)
-# >	1d10+2
-# 9 (7, +2)
-# >	2d38-1
-# 32 (20, 13, -1)
-
-dices = input().split('d')
+# Create by Maxim Ponizov and Alexandr Volodkin, group BVT 2152
 
 
-def throwDices(inputedDices, inputedEdges):
-    if '+' in "".join(dices):  # Выполняется при введенном отрицательном доп числе
-        # Количество кубиков
-        dice = int(inputedDices)
-        # Разделяем количество граней и дополнительное число
-        dividedEdgesAndAdditionalNumber = inputedEdges.split("+")
+def throwDicesVarThree():
+    dices = input('Please throw some dices like 3d6: ')  # Ввод значения
 
-        # Количество граней
-        edges = int(dividedEdgesAndAdditionalNumber[0])
-        # Дополнительное число
-        additionalNumber = int(dividedEdgesAndAdditionalNumber[-1])
+    while 'd' not in list(dices[1]):  # Пока в конвертированном в список значений не будет d под индексом 1, будет продолжаться ввод и проверка значений
+        dices = input('Wrong entry, please enter something like 3d6 or 3d6+2 or 3d6-5: ')
 
-        # х - список выпавших граней, у - итоговое число
-        x, y = [], 0
-        # Добавляем случайные значения в х
-        for i in range(1, dice + 1):
-            x.append(randrange(1, edges))
-        # Складываем в у выпавшие случайные значения
-        for i in x:
-            y += i
+    if 'd' in list(dices[1]):  # Простая проверка ввода
+        if '+' in dices or '-' in dices:  # Даннный кейс выполняется если есть доп число с знаком - или +
+            innerValue = dices.split('d')
+            inputedDices, inputedEdges = innerValue[0], innerValue[1]
+            dividedEdgesAndAdditionalNumber = 0
+            x, y, lastDigit = [], 0, ''
+            dice = int(inputedDices)
 
-        # Прибавляем дополнительное число к у
-        y += additionalNumber
+            if '+' in dices:
+                dividedEdgesAndAdditionalNumber = inputedEdges.split("+")  # Разделяем количество граней и дополнительное положительное число число
+                additionalNumber = int(dividedEdgesAndAdditionalNumber[-1])  # Дополнительное число
+                y += additionalNumber  # Прибавляем дополнительное число к у
+                lastDigit = '+' + str(additionalNumber)  # Преобразовываем доп число в "+ДопЧисло"
+            elif '-' in dices:
+                dividedEdgesAndAdditionalNumber = inputedEdges.split("-")  # Разделяем количество граней и дополнительное отрицательное число число
+                additionalNumber = int(dividedEdgesAndAdditionalNumber[-1])  # Дополнительное число
+                y -= additionalNumber  # Прибавляем дополнительное число к у
+                lastDigit = '-' + str(additionalNumber)  # Преобразовываем доп число в "+ДопЧисло"
 
-        # Преобразовываем доп число в "+ДопЧисло"
-        lastDigit = '+' + str(additionalNumber)
+            edges = int(dividedEdgesAndAdditionalNumber[0])  # Количество граней
 
-        # Преобразовываем х из [число, число] в "число, число", при этом список станет строкой
-        printableList = str(x).replace('[', '').replace(']', '')
+            for i in range(1, dice + 1):  # Добавляем случайные значения в х
+                x.append(randrange(1, edges))
+            for i in x:  # Складываем в у выпавшие случайные значения
+                y += i
 
-        # Вывод в консоль результата
-        print(y, ' (', printableList, ", ", lastDigit, ')', sep='')
+            printableList = str(x).replace('[', '').replace(']', '')  # Преобразовываем х из [число, число] в "число, число", при этом список станет строкой
 
-    elif '-' in ''.join(dices):  # Выполняется при введенном отрицательном доп числе
-        # Количество кубиков
-        dice = int(inputedDices)
-        # Разделяем количество граней и дополнительное число
-        dividedEdgesAndAdditionalNumber = inputedEdges.split("-")
+            print(y, ' (', printableList, ", ", lastDigit, ')', sep='')  # Вывод в консоль результата
 
-        # Количество граней
-        edges = int(dividedEdgesAndAdditionalNumber[0])
-        # Дополнительное число
-        additionalNumber = int(dividedEdgesAndAdditionalNumber[-1])
+        else:  # Данный кейс выполняется если нет доп числа со знаком
+            innerValue = dices.split('d')
+            inputedDices, inputedEdges = innerValue[0], innerValue[1]
 
-        # х - список выпавших граней, у - итоговое число
-        x, y = [], 0
-        # Добавляем случайные значения в х
-        for i in range(1, dice + 1):
-            x.append(randrange(1, edges))
-        # Складываем в у выпавшие случайные значения
-        for i in x:
-            y += i
+            x, y = [], 0  # х - список выпавших граней, у - итоговое число
 
-        # Отнимаем дополнительное число к у
-        y -= additionalNumber
-        # Добавляем доп отрицательное число в список
-        x.append(-additionalNumber)
-        # Преобразовываем х из [число, число] в "(число, число)", при этом лист станет строкой
-        printedList = str(x).replace('[', '(').replace(']', ')')
-        # Вывод в консоль результата
-        print(y, printedList)
+            for i in range(1, int(inputedDices) + 1):  # Добавляем случайные значения в х
+                x.append(randrange(1, int(inputedEdges)))
+            for i in x:  # Складываем в у выпавшие случайные значения
+                y += i
 
-    else:  # Кейс выполняется в случае, если нет Дополнительного числа
-        # х - список выпавших граней, у - итоговое число
-        x, y = [], 0
-        # Добавляем случайные значения в х
-        for i in range(1, int(inputedDices) + 1):
-            x.append(randrange(1, int(inputedEdges)))
-        # Складываем в у выпавшие случайные значения
-        for i in x:
-            y += i
-        # Преобразовываем х из [число, число] в "(число, число)", при этом лист станет строкой
-        printedList = str(x).replace('[', '(').replace(']', ')')
-        # Вывод в консоль результата
-        print(y, printedList)
+            printedList = str(x).replace('[', '(').replace(']', ')')  # Преобразовываем х из [число, число] в "(число, число)", при этом лист станет строкой
+            print(y, printedList)  # Вывод в консоль результата
 
 
-throwDices(dices[0], dices[1])
+throwDicesVarThree()
